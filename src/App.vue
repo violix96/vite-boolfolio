f8fe5559a3086ce4d5744d57ef65b6661a8499b9
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-07-30 20:07:26 +0200

    [UPD] made the api call to the laravel endpoint and retrieved all the projects with printing of the results in the console

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,50 @@
-<script></script>
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Projects',
+  data() {
+    return {
+      title: 'Projects',
+      projects: [],
+    };
+  },
+  methods: {
+    getProjects() {
+      const result = axios.get('http://127.0.0.1:8000/api/projects')
+        .then(response => {
+          console.log(response);
+
+          if (response.data.status && response.data.results.length) {
+            console.log('gestisco i risultati della risposta');
+            console.log(response.data.results)
+
+            this.projects = response.data.results
+
+          } else {
+            console.log('qualcosa è andato a buon fine');
+          }
+
+        }).catch(error => console.log(error));
+      console.log(result)
+
+    }
+  },
+  created() {
+    this.getProjects();
+  }
+}
+</script>
 
 <template>
   <div class="container">
-    <h1>APP</h1>
-    <div class="row">
-      <div class="col">
-        <font-awesome-icon :icon="['fas', 'user-secret']" />
-      </div>
-      <div class="col">Colonna</div>
-      <div class="col">Colonna</div>
-      <div class="col">Colonna</div>
-      <div class="col">Colonna</div>
-      <div class="col">Colonna</div>
-    </div>
+    <header>header</header>
+
+    <main>
+      <h1>{{ title }}</h1>
+    </main>
+
+    <footer>footer</footer>
   </div>
 </template>
 
